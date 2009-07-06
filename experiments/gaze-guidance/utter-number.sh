6e04022f60d5031a9697cfb86f23739fb309960c
xfred <xfred@ridley.(none)>
2009-07-06 19:43:52 +0100

    added support for better voice (mbrola)

--- experiments/gaze-guidance/utter-number.sh
+++ experiments/gaze-guidance/utter-number.sh
@@ -1,6 +1,7 @@
 DELAY=5
 TRIES=50
 RESFILE=sequence-`date +%H%M%S`.txt
+VOICE=voice_us1_mbrola # or empty
 
 echo "writting number sequence to $RESFILE"
 
@@ -8,7 +9,7 @@ i=0
 while [ $i -lt $TRIES ]
         do i=$(($i+1))
         N=`echo "$RANDOM*99/32767" | bc`
-        echo "#$i - $N"; echo $N | festival --tts
+        echo "#$i - $N"; echo "($VOICE) (SayText $N)" | festival --pipe
         echo $N >> $RESFILE
         sleep $DELAY
 done
