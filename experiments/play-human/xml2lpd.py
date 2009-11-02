af495c0a8c7ab075feb650b0757aaf9ad12d578f
fred <frederic.delaunay@plymouth.ac.uk>
2009-11-02 17:13:04 +0000

    added eyes values

--- experiments/play-human/xml2lpd.py
+++ experiments/play-human/xml2lpd.py
@@ -8,82 +8,94 @@ import xml.parsers.expat
 import sys
 
 
-class Dialogue():
-    """Static Class, we don't really need to create an instance."""
-    fps = None
-    # each data is a dictionnary with time as a key (in s.)
-    eye_data = {}       # value: ()
-    face_data = {}      # value: () 
-    head_data = {}      # value: ()
-    speech_data = {}    # value: ()
-
-
+fps = None
+human_data = {}       # key: time(in ms), value specific to head/eye/face/..: ()
 
 def set_dialogue(attrs):
-    Dialogue.fps = int(attrs['fps'])
+    global fps
+    fps = int(attrs['fps'])
 
 def set_cstate(attrs):
     pass
 
 def set_phgs(attrs):
     """Get orientation axis and angle."""
-    value = ( int(attrs['direction']), None )
-    Dialogue.head_data[float(attrs['startTime'])/Dialogue.fps] = value
+    value = 'head %i' % (int(attrs['direction']), None)
+    human_data[float(attrs['startTime'])/fps] = value
 
 def set_pems(attrs):
     """Eye Orientation."""
-    value = ( int(attrs['angle']), float(attrs['distance']) )
-    Dialogue.eye_data[float(attrs['startTime'])/Dialogue.fps] = value
+    value = 'eyes %i %.2f' % (int(attrs['angle']), float(attrs['distance']))
+    human_data[float(attrs['startTime'])/fps] = value
 
+
+
+verbose = False
 functions = {
-#             "cState": set_cstate,
-             "phgs": set_phgs,
-             "pems": set_pems,
-             "dialogue": set_dialogue
-             }
+    #        "cState": set_cstate,
+    #        "phgs": set_phgs,
+        "pems": set_pems,
+        "dialogue": set_dialogue
+        }
 
 # main handler functions
 def start_element(name, attrs):
     try:
         functions[name](attrs)
-        print name, attrs
+        if verbose:
+            print name, attrs
     except KeyError, e:
-#        print "[unimplemented %s]" % e
-        pass
+        name = "[unimplemented %s]" % e
+        if verbose > 1:
+            print name, attrs
 
 def end_element(name):
     pass
 
+
 def char_data(data):
     data =  data.strip()
-#    if (data):
-#        print '`--', repr(data)
-
+    #    if (data):
+    #        print '`--', repr(data)
 
 
 def main():
     try:
-        xml_file = file(sys.argv[1])
+        global verbose
+        verbose = sys.argv[1][0] == '-' and sys.argv[1].count('v')
+        if verbose:
+            sys.argv.pop(1)
+        xml_fname = sys.argv[1]
+        out_fname = len(sys.argv) > 2 and sys.argv[2]
     except IndexError:
-        print "xml file argument needed"
+        print sys.argv[0],": [-v[v] (verbose)] xml_file out_file (or none for stdout)"
         exit(-1)
 
-    try:
-        encoding = sys.argv[2]
-    except IndexError:
-        encoding = None
-
-    parser = xml.parsers.expat.ParserCreate(encoding)
+    parser = xml.parsers.expat.ParserCreate()
     parser.buffer_text = True
-    parser.StartElementHandler = start_element
-    parser.EndElementHandler = end_element
+    parser.StartElementHandler  = start_element
+    parser.EndElementHandler    = end_element
     parser.CharacterDataHandler = char_data
     try:
+        xml_file = file(xml_fname)
         parser.ParseFile(xml_file)
     except Exception, e:
         print "Something's wrong with l."+str(parser.CurrentLineNumber), ":", e
         exit(-1)
-    xml_file.close()
+        
+    try:
+        out_file = out_fname and open(out_fname, 'w') or sys.stdout
+    except IOError:
+        print "could not open file", out_fname, "using standard output"
+        out_file = sys.stdout
 
+    for time in sorted(human_data.keys()):
+        data= human_data[time]
+        line = "%i : %s\n" % (time*100, data)
+        out_file.write(line)
+    if out_fname :
+        out_file.close()
+    print "done"
 
+        
 __name__ == '__main__' and main()
