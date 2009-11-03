1abed22b42e45ac9640f44790d546a997bcad3a8
fred <frederic.delaunay@plymouth.ac.uk>
2009-11-03 16:58:08 +0000

    fixed architecture for the player: a main class for sync and buffering; subclasses as remotes for submodules.

--- experiments/play-human/human_player.py
+++ experiments/play-human/human_player.py
@@ -12,67 +12,67 @@ import sys
 import comm
 import conf
 
-X_FACTOR=1
-X_OFFSET=0
-Y_FACTOR=1
-Y_OFFSET=0
-Z_FACTOR=1
-Z_OFFSET=0
+
+
 
 class GazeConnection(comm.BasicHandler):
-	"""Connection to gaze server"""
-
-	process = comm.process
+  """Connection to gaze server"""
+  process = comm.process
 	
-	def __init__(self):
-		comm.BasicHandler.__init__(self)
-		self.focus_pos = (0., -5., 0.)
-		self.diameter = .5	# normalized
+  def __init__(self):
+    comm.BasicHandler.__init__(self)
+    self.focus_pos = (0., -5., 0.)
+    self.diameter = .5	# normalized
+    
+    # force blocking using timeout
+    self.connect_to(conf.gaze_addr, 3)
+    if not self.connected:
+	comm.LOG.warning("gaze player could not connect!")
 
-		# force blocking using timeout
-		self.connect_to(conf.gaze_addr, 3)
-                if not self.connected:
-                        comm.LOG.warning("gaze player could not connect!")
-
-	def send_gaze(self, direction, distance, speed):
-		"""Set relative eye movements: orientation for both eyes."""
-		self.send()
+  def send_gaze(self, direction, distance, speed):
+      """Set relative eye movements: orientation for both eyes."""
+      self.send()
 
 
 class HeadConnection(comm.BasicHandler):
-	"""Connection to head server"""
+  """Connection to head server"""
+  def __init__(self):
+    comm.BasicHandler.__init__(self)
+    self.connect_to(conf.head_addr, 3)
+    if not self.connected:
+      comm.LOG.warning("head player could not connect!")
 
-	def __init__(self):
-		comm.BasicHandler.__init__(self)
-		self.connect_to(conf.head_addr, 3)
-		if not self.connected:
-			comm.LOG.warning("head player could not connect!")
+  def send_head(self):
+    """Set relative head movement."""
+    pass
 
-	def send_head(self):
-		"""Set relative head movement."""
-		pass
 
 class Player():
-	"""Reads a data file got from xml2ldp.py and"""
-	""" sends contents to appropriate endpoints."""
-
-	def __init__(self):
-		self.gaze, self.head = GazeConnection(), HeadConnection()
+  """Reads a data file got from xml2ldp.py and"""
+  """ sends contents to appropriate endpoints."""
+  def __init__(self):
+    self.gaze, self.head = GazeConnection(), HeadConnection()
 		
+  def read_and_play(self, file):
+    f = open(file)
+    for line in f.readlines():
+      time, cmd = line.split(':')
+      args = cmd.split()
+      print args
+      try:
+        fct = getattr(self, "parse_"+args[0])
+      except AttributeError:
+        print "command not available:", args[0]
 
-	def read_and_play(self, file):
-		f = open(file)
-		for line in f.readlines():
-			info = line.split()
-			self.head.send_neck()
-			self.gaze.send_gaze()
+      fct(args[1:])
 
+  def parse_eyes(
 
 
 if __name__ == "__main__":
-	p = Player()
-	try:
-		p.read_and_play(sys.argv[1])
-	except IndexError:
-		print "pld data file ?"
+  p = Player()
+  try:
+    p.read_and_play(sys.argv[1])
+  except IndexError:
+    print "pld data file ?"
 
