00c47a41b788bf0a266c3348edc9193f91174411
fred <frederic.delaunay@plymouth.ac.uk>
2009-11-03 16:54:14 +0000

    support for orientation command + command echoing keyword + completed orientation stuff + additional debug

--- HRI/gaze/__init__.py
+++ HRI/gaze/__init__.py
@@ -26,10 +26,23 @@ class GazeClient(comm.RemoteClient):
                 args = [ float(arg) for arg in argline.split() ]
                 self.server.set_focus(tuple(args[:3]))
             except Exception, e:
-                LOG.warning("[focus] bad argument line: '%s', caused: %s" %
+                LOG.warning("[focus] bad argument line:'%s', caused: %s" %
                             (argline,e) )
         else:
-            self.send_msg(str(self.server.focus))
+            self.send_msg("focus",str(self.server.focus))
+
+    def cmd_orientation(self, argline):
+        """args: if empty, returns current orientation. Otherwise, set it."""
+        if len(argline):
+            try:
+                args = [ float(arg) for arg in argline.split() ]
+                self.server.set_orientation(
+                    tuple([args[0:3],args[3:6],args[6:9]]) )
+            except Exception, e:
+                LOG.warning("[orientation] bad argument line:'%s', caused: %s" %
+                            (argline,e) )
+        else:
+            self.send_msg("orientation",str(self.server.angles))
 
     def cmd_shutdown(self, args):
         """args: unused."""
@@ -49,26 +62,32 @@ class Gaze(comm.BasicServer):
     def __init__(self, addr_port):
         comm.BasicServer.__init__(self, GazeClient)
         self.listen_to(addr_port)
-        self.focus = (.0, -1.0, .0)       # default to reasonably close
-        self.angles = (0, 0, 0)
+        self.focus = (.0, -1.0, .0)
+        self.orientation = ([0,0,0], [0,-1,0], [0,0,0])
+        self.changed = False            # update flag ('f', 'o')
 
     def set_focus(self, pos):
         """Set focus (1 vector3)."""
         self.focus = pos
+        self.changed = 'f'
         self.send_focus()
         
     def set_orientation(self, matrix):
         """Just set eyes orientation (3 vector3)."""
-        self.angles = angles
+        self.orientation = matrix
+        self.changed = 'o'
         self.send_orientation()
 
     def send_focus(self):
+        """Echo focus command to clients"""
         for cl in self.get_clients():
             cl.send_msg("focus %f %f %f" % self.focus )
 
     def send_orientation(self):
-        #TODO: complete this !
-        pass
+        """Echo command to clients"""
+        for cl in self.get_clients():
+            cl.send_msg("orient "+ str(self.orientation) )
+        
 
 
 if __name__ == '__main__':
