7e2a30b95dfa9c28b56ccd8473b234b2fc3708a4
xfred <frederic.delaunay@plymouth.ac.uk>
2009-11-23 01:29:55 +0000

    use default conf if no env + remove old unix sockets

--- face.sh
+++ face.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 FACE_BIN="lightbot_face"
 
@@ -16,8 +16,22 @@ else
     BGE_PYTHON_VERS=2.5
     BGE_PYTHON_PATH=/usr/lib/python$BGE_PYTHON_VERS/:/usr/lib/python$BGE_PYTHON_VERS/lib-dynload
 
+# set standard conf if not set yet
+    if test -z "$LIGHTBOT_CONF" ; then
+        export LIGHTBOT_CONF=./common/lightbot.conf
+    fi
+
+# remove old unix sockets if present
+    SOCKETS=`PYTHONPATH=$BGE_SCRIPT_PATH python -c 'import conf; conf.get_unix_sockets(1)'`
+    if test -n "$SOCKETS" ; then
+        echo "deleting old sockets"
+        for s in $SOCKETS; do
+            if test -S "$s"; then rm -v "$s"; fi
+        done
+    fi
+
 # Now launch
     echo -n "launching face "
     if [ $# -ge 1 ]; then echo "using options: $@"; else echo ""; fi
-    PYTHONPATH=$PYTHONPATH:$BGE_PYTHON_PATH:$BGE_SCRIPT_PATH ./$FACE_BIN $@
+    PYTHONPATH="$PYTHONPATH:$BGE_PYTHON_PATH:$BGE_SCRIPT_PATH" ./$FACE_BIN $@
 fi
