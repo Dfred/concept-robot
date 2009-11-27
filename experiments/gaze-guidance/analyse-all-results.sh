c183a5b3de043860d4fe691a838fa25e702a4b6b
fred <frederic.delaunay@plymouth.ac.uk>
2009-11-27 18:36:03 +0000

    cosmetics

--- experiments/gaze-guidance/analyse-all-results.sh
+++ experiments/gaze-guidance/analyse-all-results.sh
@@ -15,12 +15,12 @@ fi
 
 for DISP in "dome" "flat" "human" "mask"
 do 
-    echo processing results for $DISP #in file result-$DISP.txt
+    echo "processing results for $DISP" #in file result-$DISP.txt
     for SEQ in $RES_DIR/sequence-$DISP-*
     do
-        PAIR=`echo $SEQ | cut -d- -f3`
+        PAIR=`basename $SEQ | cut -d- -f3`
         if [ $PAIR == '.txt' ]; then PAIR="A-Z"; fi
-#        echo -n "$PAIR "
+#        echo -n "$PAIR"
         for RES in $RES_DIR/$DISP-[$PAIR]-*
         do
             ./analyse-results.py $SEQ $RES $@ #>> result-$DISP.txt
