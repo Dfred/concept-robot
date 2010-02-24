990d68ee24c82ae8b48fe1314a11466c6d54a1f6
unknown <concept@.(none)>
2010-02-24 00:11:49 +0000

    support for Windows with MinGW

--- face.sh
+++ face.sh
@@ -1,40 +1,55 @@
-#!/bin/bash
+#!/bin/sh
 
+BGE_PYTHON_VERS=2.5
 FACE_BIN="lightbot_face"
 
-if ! test -x ./$FACE_BIN; then
-    echo
-    echo "Could not find executable file '$FACE_BIN' in this directory."
-    echo
-else
+check()
+{
+  if ! $1; then
+	echo
+	echo $2
+	exit -1
+  fi
+}
 
-    BGE_SCRIPT_PATH=:common/:HRI:HRI/face
+check "test -x ./$FACE_BIN" "Could not find executable file '$FACE_BIN' in this directory."
+check "python -c 'print'"  "python not found. Did you set your PATH ?"
 
-# The following depends on internal BGE python version
-
-    BGE_PYTHON_VERS=2.5
-    BGE_PYTHON_PATH=/usr/lib/python$BGE_PYTHON_VERS/:/usr/lib/python$BGE_PYTHON_VERS/lib-dynload
+# Platform dependent paths (handles the famous nagger)
+case `uname -s` in
+	MINGW*)
+	MODULES_PATH='common;HRI;HRI/face'
+	PYTHONPATH="$MODULES_PATH"
+	;;
+	*)
+	MODULES_PATH='common:HRI:HRI/face'
+        # The following depends on the BGE python version (necessary for blender 2.4x)
+        BGE_PYTHON_PATH="/usr/lib/python$BGE_PYTHON_VERS/:/usr/lib/python$BGE_PYTHON_VERS/lib-dynload"
+        PYTHONPATH="$PYTHONPATH:$BGE_PYTHON_PATH:$MODULES_PATH"
+;;
+esac
+echo $PYTHONPATH
+export PYTHONPATH
 
 # set standard conf if not set yet
-    if test -z "$LIGHTBOT_CONF" ; then
-        export LIGHTBOT_CONF=./common/lightbot.conf
-    fi
+if test -z "$LIGHTBOT_CONF" ; then
+	export LIGHTBOT_CONF=./common/lightbot.conf
+fi
 
 # remove old unix sockets if present
-    SOCKETS=`PYTHONPATH=$BGE_SCRIPT_PATH python -c 'import conf; conf.get_unix_sockets(1)'`
-    if test $? -ne 0; then
+SOCKETS=`python -c 'import conf; conf.get_unix_sockets(1)'`
+if test $? -ne 0; then
 	echo "ERROR: Failure to get socket list !"
 	exit -1
-    fi
-    if test -n "$SOCKETS" ; then
-        echo "deleting old sockets: "
-        for s in $SOCKETS; do
-            if test -S "$s"; then echo "  "`rm -v "$s"` ; fi
-        done
-    fi
+fi
+if test -n "$SOCKETS" ; then
+	echo "deleting old sockets: "
+	for s in $SOCKETS; do
+	  if test -S "$s"; then echo "  "`rm -v "$s"` ; fi
+	done
+fi
 
 # Now launch
-    echo -n "launching face "
-    if [ $# -ge 1 ]; then echo "using options: $@"; else echo ""; fi
-    PYTHONPATH="$PYTHONPATH:$BGE_PYTHON_PATH:$BGE_SCRIPT_PATH" ./$FACE_BIN $@
-fi
+echo -n "--- launching face "
+if [ $# -ge 1 ]; then echo "using options: $@"; else echo ""; fi
+./$FACE_BIN $@
