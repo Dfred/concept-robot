d3e490f0020b7a4831c89fde5fcaf25cee4332c6
unknown <concept@.(none)>
2010-03-22 20:38:56 +0000

    comm now uses SocketServer

--- common/comm/__init__.py
+++ common/comm/__init__.py
@@ -39,19 +39,19 @@ __date__    = "$$"
 import re
 import os
 import logging
-import asyncore
+import SocketServer
+import socket
+import select
 
-# importing our modules. They look at logging basic configuration
-from basic_server import BasicHandler, BasicServer
+# save users from importing socket
+error = socket.error
 
-
-# deal with logging 1st
+# create our logging object and set log format
 LOG = logging.getLogger("comm")
-FORMAT = "%(filename)s[%(lineno)d] -%(levelname)s- %(message)s"
+FORMAT = "%(filename)s[%(lineno)d] -%(levelname)s-\t%(message)s"
 
 # Set basic config of logger for client modules not dealing with logging.
-logging.basicConfig(level=logging.WARNING, format=FORMAT)
-
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 
 class NullHandler(logging.Handler):
@@ -62,6 +62,11 @@ class NullHandler(logging.Handler):
     def emit(self, record):
         pass
 
+class CmdError(Exception):
+    """Base Exception class for error in command processing.
+    Failed commands shall raise Exceptions inherting from this class.
+    """
+    pass
 
 
 # now our specifics
@@ -74,129 +79,261 @@ RE_PORT   = r'(?P<PORT>\d{2,5}|[\w/\.]+[^/])'
 CRE_CMDLINE_ARG = re.compile('('+RE_NAME+'=)?'+RE_ADDR+':'+RE_PORT)
 CRE_PROTOCOL_SYNTAX = re.compile("\s*\w+(\s+\w+)*")
 
-# just a way to get rid of the asyncore module.
-loop = asyncore.loop
 
 
-class CmdError(Exception):
-    """Base Exception class for error in command processing.
-    Failed commands shall raise Exceptions inherting from this class.
-    """
-    pass
+def createBaseServer(addr_port, cnx_handler):
+    """Creates the appropriate server (from SocketServer) according to addr_port"""
 
-def create(cmdline, remotes, server_class):
+    addr, port = addr_port
+    if type(port) == type(42):
+        server_class = SocketServer.ThreadingTCPServer
+    elif type(port) == type("") and addr in ["127.0.0.1", "localhost"]:
+        if hasattr(SocketServer, "ThreadingUnixStreamServer"):
+            server_class = SocketServer.ThreadingUnixStreamServer
+        else:
+            raise Exception("TODO: named pipes for windows")
+    else:
+        raise Exception("Could not get server type from addr_port info")
+    return server_class(addr_port, cnx_handler)
+
+
+#XXX: so far, creation from commandline is used only by comm_example
+def create(cmdline, remoteClient_class, server_class, cnx_handler):
     """Parses cmdline and instanciates given classes.
-    Note that any matching argument will be swallowed wether or not a
-     successful instanciation has been made
+    Note that any matching argument will be swallowed whether a successful
+     instanciation has been made or not.
 
      cmdline: user command line checked for arguments
-     remotes: { "module_name" : client_class, ... }
+     remoteClient_class: { "module_name" : client_class, ... }
      server_class: last instance returned only (see _parse_args())
     Returns (unused arguments, [client_class objects], server_class object)
     """
+
+    def parse_args(args):    
+        """Parses arguments of the form: name=ip_address_or_fqdn:port
+         'name' indentifies 'ip_address' as a remote server to connect to,
+         if 'name=' is ommited 'ip_address' identifies a local interface used to
+         listen to remote connections.
+
+         args: an array of strings (as returned by sys.argv).
+        Returns (unused arguments, [(name, addr, port), ...] )
+        """
+        unused, infos = [], []
+        for arg in args:
+            m = CRE_CMDLINE_ARG.match(arg)
+            if m == None:
+                unused.append(arg)
+            else:
+                port = m.group("PORT")
+                if port.isdigit():
+                    port = int(port)
+                infos.append((m.group("NAME"), m.group("ADDR"), port))
+        return (unused, infos)
+
     clients = []
     server = None
     unused, infos = parse_args(cmdline)
     for name, address, port in infos:
         if name == None:
-            server = server_class((address, port))
+            server = server_class((address, port), cnx_handler)
         else:
             try:
-                clients.append(remotes[name]((address, port)))
+                clients.append(remoteClient_class[name]((address, port)))
             except KeyError:
                 print "warning: found unmatched name '"+name+"' in cmdline"
     return (unused, clients, server)
 
-
-def parse_args(args):    
-    """Parses arguments of the form: name=ip_address_or_fqdn:port
-     'name' indentifies 'ip_address' as a remote server to connect to,
-     if 'name=' is ommited 'ip_address' identifies a local interface used to
-     listen to remote connections.
-
-     args: an array of strings (as returned by sys.argv).
-    Returns (unused arguments, [(name, addr, port), ...] )
-    """
-    unused = []
-    infos = []
-    for arg in args:
-        m = CRE_CMDLINE_ARG.match(arg)
-        if m == None:
-            unused.append(arg)
-        else:
-            port = m.group("PORT")
-            if port.isdigit():
-                port = int(port)
-            infos.append((m.group("NAME"), m.group("ADDR"), port))
-    return (unused, infos)
         
 
-def process(self, command):
-    """Main function: command dispatcher for incoming commands.
-    If available, calls the cmd_ function defined in self according to command.
-    Simultaneous commands (same step) can be linked with '&&'.
-
-    command: string of text to be matched with CRE_PROTOCOL_SYNTAX
-    Returns the number of bytes read.
-    """
-    length = len(command)
-    command = command.strip()
-    LOG.debug("%s> command (%iB): '%s'", self.id, len(command), command)
-    if len(command) == 0 or command[0] == '#':
-        return length
+class BaseComm:
+    """Basic protocol handling and command-to-function resolver.
+       Not to be instancied"""
     
-    for cmdline in command.split('&&'):
-        if not CRE_PROTOCOL_SYNTAX.match(cmdline):
-            LOG.info("%s> syntax error : '%s'", self.id, cmdline)
-            return length
-        
-    cmd_tokens = cmdline.split(None,1) # keep 1
-    cmd, args = ("cmd_"+cmd_tokens[0],
-                 len(cmd_tokens)>1 and cmd_tokens[1] or "")
-    try:
-        ret = None
-        if cmd in dir(self):
-            exec("ret = self."+cmd+"(args)")
-        else:
-            LOG.info("%s> command not found '%s'", self.id, command)
-    except CmdError, e:
-        LOG.warning("%s> unsuccessful command '%s' [%s]", self.id, cmdline, e)
-    return length
-
-
-class RemoteClient(BasicHandler):
-    """BasicHandler objects are instancied by BasicServer itself.
-    Adds support for general syntax checking, cmd_ functions, 
-     default cmd_clients and cmd_shutdown functions
-    """
-
-    def __init__(self):
-        BasicHandler.__init__(self)
+    def handle_notfound(self, cmd):
+        """When a method (a command) is not found in self. See process()."""
+        pass
 
     def process(self, command):
-        return process(self, command)
+        """Command dispatcher function common to client and handler."""
+        """Tokenize command and calls 'cmd_ + 1st_token' function defined in self.
+         Remaining tokens are given as argument in an array. self.handle_notfound is
+         called if the built function name doesn't exist in self. Simultaneous
+         commands (called within the same step) can be issued linking them with '&&'.
+
+        command: string of text to be matched with CRE_PROTOCOL_SYNTAX
+        Returns the number of bytes read.
+        """
+        length = len(command)
+        LOG.debug("%s> command [%iB]: '%s'", self.cnx.fileno(), length, command)
+        if not length or command.startswith('#'):
+            return length
+
+        for cmdline in command.split('&&'):
+            if not CRE_PROTOCOL_SYNTAX.match(cmdline):
+                LOG.info("%s> syntax error : '%s'", self.cnx.fileno(), cmdline)
+                return length
+            
+        cmd_tokens = cmdline.split(None,1) # keep 1
+        cmd, args = "cmd_"+cmd_tokens[0], cmd_tokens[1:]
+        try:
+            ret = None
+            if cmd in dir(self):
+                exec("ret = self."+cmd+"(args)")
+            else:
+                LOG.info("%s> command not found '%s'", self.cnx.fileno(), command)
+                self.handle_notfound(cmd)
+        except CmdError, e:
+            LOG.warning("%s> unsuccessful command '%s' [%s]", self.cnx.fileno(), cmdline, e)
+        return length
+
+    def send_msg(self, msg):
+        """Sends msg with a trailing \\n as required by the protocol."""
+        LOG.debug("sending %s\n", msg)
+        self.cnx.send(msg+'\n')
+
+    def cmd_bye(self, args):
+        """Disconnects that client."""
+        self.running = False
+    cmd_EOF = cmd_bye
         
-    def cmd_clients(self, args):
-        """Lists clients currently connected"""
-        LOG.debug("%s> listing %i clients", self.id, len(asyncore.socket_map)-1)
-        clients_infos = []
-        for cl in asyncore.socket_map.values():
-            if not cl.connected:
-                continue
-            clients_infos.append( type(cl.addr) == type("") and
-                                  (cl.id, "UNIX", "localhost") or
-                                  (cl.id, cl.addr[1], cl.addr[0]) )
-        clients_infos.sort()
-        self.obuffer += "connected_clients %i: (ID, PORT, ADDR)" % (
-            len(clients_infos))
-        for client_info in clients_infos:
-            self.obuffer += "\\\n> %s:  %5s  %s" % client_info
-        self.send_msg("")
-        
+
+class RequestHandler(BaseComm, SocketServer.StreamRequestHandler, object):
+    """Instancied on successful connection to the server: a remote client."""
+    """Adds support for general syntax checking, and default functions :
+     cmd_shutdown, cmd_clients and cmd_verb.
+    """
+    def setup(self):
+        super(RequestHandler,self).setup()
+        if not hasattr(self.server, "clients"):
+            self.server.clients = []
+        self.server.clients.append(self)
+
+    def finish(self):
+        super(RequestHandler,self).setup()
+        self.server.clients.remove(self)
+
+    def handle(self):
+        """entry point for processing client commands.
+        self.rfile and self.wfile are streams and use standard file interface.
+        read is buffered, write is not.
+        """
+        LOG.info("connection accepted from %s" % self.client_address[0])
+        self.cnx = self.request # for process()
+        self.running = True
+        command = ""
+        while self.running:
+            try:
+                command += self.rfile.readline().strip()
+                LOG.debug("readline().strip(): [%i] %s", len(command), command)
+            except socket.error, e:
+                LOG.error(e)
+                LOG.error("comm channel broken, pruning client %s", self.client_address[0])
+                return
+            if command.endswith('\\'):
+                command = command[:-1]
+            command = command[self.process(command):]
+        print "au revoir"
+
     def cmd_shutdown(self, args):
         """Disconnects all clients and terminate the server process"""
         if self.server == None:
             raise CmdError("cannot shutdown server")
-        LOG.debug("%s> stopping server", self.id)
-        self.server.shutdown("shutdown from server: disconnecting all clients")
+        LOG.info("%s> stopping server", self.cnx.fileno())
+        self.server.shutdown()
+        self.running = False
 
+    def cmd_clients(self, args):
+        """Lists clients currently connected.
+        We want other clients to be able to inquiry so server holds the info.
+        """
+        LOG.info("%s> listing %i clients.", self.cnx.fileno(), len(self.server.clients))
+        clients_infos = []
+        for cl in self.server.clients:
+            clients_infos.append( type(cl.client_address) == type("") and
+                                  (cl.cnx.fileno(), "UNIX", "localhost") or
+                                  (cl.cnx.fileno(), cl.client_address[1],
+                                                    cl.client_address[0]) )
+        clients_infos.sort()
+        obuffer = "clients : %i connected: (ID, PORT, ADDR)" % (len(clients_infos))
+        for client_info in clients_infos:
+            obuffer += "\\\n> %s:  %5s  %s" % client_info
+        self.send_msg(obuffer)
+        
+    def cmd_verb(self, args):
+        """Changes LOG verbosity level."""
+        if not args:
+            self.send_msg("CRITICAL 	50\nERROR 	40\nWARNING 30\n"
+                          "INFO 	20\nDEBUG 	10\nNOTSET  0")
+        else:
+            LOG.info("changing log level to %s", args[0])
+            LOG.setLevel(args[0])
+
+
+class BaseClient(BaseComm):
+    """Client creating a connection to a (remote) server.
+       Remember it's impossible to use the file interface of the socket while
+        setting a socket timeout.
+    """
+    def __init__(self, addr_port):
+        self.target_addr = addr_port
+        self.cnx = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
+
+    def set_timeout(self, timeout):
+        self.cnx.settimeout(self.timeout)
+
+    def connect_and_run(self):
+        try:
+            self.cnx.connect(self.target_addr)
+        except socket.error, e:
+            self.handle_error(e)
+            self.cnx.close()
+            return
+        self.connected = True
+        self.handle_connect()
+
+        self.read_until_done()
+
+        self.cnx.close()
+        self.connected = False
+        self.handle_disconnect()
+
+    def read_until_done(self):
+        """Wait, read and process data, calling self.handle_timeout when self.timeout elapsed."""
+
+        def abort(self):
+            LOG.debug("communication with server has been interrupted")
+            self.running = False
+
+        self.running = True
+        line = ""
+        while self.running:
+            fd_sets = select.select([self.cnx], [], [], self.cnx.gettimeout())
+            if not fd_sets[0]:
+                self.handle_timeout()
+            if fd_sets[2]:
+                abort(self)
+                self.handle_error()
+            else:
+                try:
+                    buff = self.cnx.recv(1024)
+                except socket.error, e:
+                    self.handle_error(e)
+                    return
+                if not buff:
+                    abort(self)
+            line += buff
+            line = line[self.process(line):]
+
+    def handle_connect(self):
+        pass
+
+    def handle_error(self, e):
+        raise e
+        pass
+
+    def handle_disconnect(self):
+        pass
+
+    def handle_notfound(self, cmd):
+        pass
--- common/comm_example.py
+++ common/comm_example.py
@@ -35,104 +35,76 @@ import comm
 logging.basicConfig(level=logging.INFO, format=comm.FORMAT)
 
 
-class Client(comm.BasicHandler):
-    """Our connection to a target server"""
+class PrintClient(comm.BaseClient):
+    """A client printing anything read."""
+    """As process() is overriden, no cmd_ functions are defined here."""
 
     def __init__(self, addr_port):
-        print "I'm a Client !"
-        comm.BasicHandler.__init__(self)
-
-        # we're asking for our client to connect to server, but we don't know
-        # when this will happen, so rely on handle_connect to be noticed.
-        self.connect_to(addr_port)
-
-    def handle_connect(self):
-        """Called when the client has just connected successfully"""
-        if comm.BasicHandler.handle_connect(self):
-            print self.__repr__(), "now connected to", self.addr
-        else:
-            print self.__repr__(), "failed to connect to", self.addr
-
-
-class EchoClient(Client):
-    """A client inheriting from Client, and printing anything read"""
-
-    def __init__(self, addr_port):
-        Client.__init__(self, addr_port)
-        self.stay_connected = True
+        """This constructor exists just to show what's happening in runtime"""
+        print "I'm connecting and printing out incoming server data!"
+        comm.BaseClient.__init__(self, addr_port)
 
     # we define the process method that handles incoming data (line by line).
     def process(self, line):
         print "received:",line
         return len(line)
 
+    # e is an exception (socket.error)
+    def handle_error(self, e):
+        print "ERROR", e
 
-class ClientFoo(Client):
+
+class ClientFoo(comm.BaseClient):
     """Our connection to a target server, pong-aware and
      shutting down the server """
 
     def __init__(self, addr_port):
-        print "I'm a ClientFoo, strongly connecting and aware of your pongZ !"
+        print "I'm connecting and aware of your pongZ !"
+        comm.BaseClient.__init__(self, addr_port)
 
-        comm.BasicHandler.__init__(self)
-        self.received_pong = False
-        # we want a synchronous connect without waiting more than 500ms,
-        # timeout allows us to retry to connect in case of connection failure.
-        for retry in xrange(3):
-            if self.connect_to(addr_port, 0.5):
-                break
-            time.sleep(1)       # let some time to startup remote components
+    def connect_and_run(self):
+        """Connect to host and enter self loop."""
+        """Function returns on disconnection."""
+        try:
+            comm.BaseClient.connect_and_run(self)
+        except comm.error, e:
+            print e
 
-        # self.connected is reliable since connect_to() was synchronous
-        if not self.connected:
-            print "failed to connect. Aborting."
-            self.close()
+    def handle_connect(self):
+        self.send_msg("hello")
+        hb = 3
+        print "sending %i heartbeats" % hb
+        while hb:
+            t = time.time()
+            print client, "heartbeat test #", hb, t
+            client.send_msg("ping %i" % t)
+            hb -= 1
+            time.sleep(1)
         self.send_msg("clients")
-
-    def handle_close(self):
+        print "sending shutdown"
         self.send_msg("shutdown")
-        comm.BasicHandler.handle_close(self)
+
+    def handle_disconnected(self):
+        """On disconnection, for clean-up purposes"""
+        print "Oh! we've been disconnected :("
 
     def cmd_pong(self, args):
         """On pong reception, we could update a counter.."""
-        self.received_pong = True
+        print "PONG from server"
 
-    def cmd_connected_clients(self, args):
+    def cmd_clients(self, args):
         """Answer for the 'clients' command is pretty-printed."""
         print args[0]+" connected client(s):"
         pprint.pprint(args.split('>')[1:])
 
-# another (rather dirty) way of defining the process method
-ClientFoo.process = comm.process
-        
 
-class Server(comm.BasicServer):
-    """The server itself, listening for incoming connections and spawning new
-     instances of ClientR (argument to comm.BasicServer's constructor).
-    """
-
-    # Here we have to match comm.BasicServer constructor
-    def __init__(self, addr_port):
-        """Look, that's where you tell the server to spawn ClientR instances"""
-        comm.BasicServer.__init__(self, ClientR)
-        try:
-            self.listen_to(addr_port)
-        except UserWarning, err:        # ok ok it's slightly dirty
-            print err
-            exit(-1)            # because we consider serving is important
-
-    def handle_accept(self):
-        print "SERVER> a remote client is trying to connect to our server."
-        comm.BasicServer.handle_accept(self)
-
-
-class ClientR(comm.RemoteClient):
+class ClientR(comm.RequestHandler):
     """A remote client, spawn by server so already connected to our server. 
     Features support for cmd_ functions, interaction.. and ping!"""
 
-    def handle_connect(self):
-        print "I'm a new Remote client connected to our server ! \\o/"
-        comm.RemoteClient.handle_connect(self)
+    def __init__(self, cnx, client_addr, server):
+        print "server accepted connection from %s (%s)" % client_addr
+        comm.RequestHandler.__init__(self, cnx, client_addr, server)
 
     def cmd_hello(self, args):
         print "SERVER> a polite client !"
@@ -144,58 +116,29 @@ class ClientR(comm.RemoteClient):
         self.send_msg("pong %i" % time.time())
 
 
-REMOTE_MODULES = { "c": Client,
-                   "echo" : EchoClient,
+REMOTE_MODULES = { "bc": comm.BaseClient,
+                   "print" : PrintClient,
                    "pong_shut": ClientFoo,
                    "do_not": ClientR    # this is stupid to do, so don't do it!
                    }
-HEARTBEATS = 3
 
 if __name__ == '__main__':
+    import sys
     print "###"
     print "### available remotes: "+str(REMOTE_MODULES.keys())
-    import sys
     print "### argv: "+str(sys.argv)
-
-    remains, clients, server = comm.create(sys.argv, REMOTE_MODULES, Server)
-    print ""
-
+    remains, clients, server = comm.create(sys.argv,
+                                           REMOTE_MODULES,
+                                           comm.createBaseServer,   # a server and
+                                           ClientR)                 # its handler
     print "### remaining args: "+str(remains)
     print "### clients: \t"+str(clients)
     print "### server: \t"+str(server)
     print ""
 
-    if server and clients:
-        print "unlimited loop started"
-        # the function will return when no more sockets are open
-        comm.loop()
-
-    elif server:     # (you *should* have only one)
-#        import pdb
-#        pdb.set_trace()
-        while server.is_readable:           # a server is not writable
-            comm.loop(5, count=1)       # block for 500ms or new data event
-
-    elif clients:
+    if clients:
+        import threading
         for client in clients:
-            if not hasattr(client, "received_pong"):
-                continue
-            # generate a bit of traffic
-            hb = HEARTBEATS
-            print "sending %i heartbeats" % hb
-            while client.connected and not client.received_pong and hb:
-                t = time.time()
-                print client.__repr__(), "heartbeat test #", hb, t
-                client.send_msg("ping %i" % t)
-                comm.loop(1, count=1)
-                hb -= 1
-                time.sleep(1)
-
-    if [ c for c in clients if isinstance(c, EchoClient) ]:
-        print "running loop for persistent clients"
-        comm.loop()
-
-    for c in clients:
-        c.close()
-
-    print "ok, we're done"
+            threading.Thread(target=client.connect_and_run).start()
+    if server:     # (you *should* have only one)
+        server.serve_forever()
