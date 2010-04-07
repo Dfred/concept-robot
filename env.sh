37b82d916b29e53db725571ac1dc8a8a525d0e0a
unknown <concept@.(none)>
2010-04-07 11:14:17 +0100

    now defaults to current directory

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 if test -z "$CONCEPT_DIR"; then
-    echo '$CONCEPT_DIR' not set, assuming '$HOME' "($HOME)"
+    echo '$CONCEPT_DIR' not set, assuming '$PWD' "($PWD)"
     CONCEPT_DIR=$HOME
 fi
 export PYTHONPATH=$CONCEPT_DIR/common/
