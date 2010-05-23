797872773bf3ddbc03c3882c52762b0383607ddb
Frédéric Delaunay <fdelauna@qatar.cmu.edu>
2010-05-23 20:16:38 +0300

    preparing unification of interface

--- HRI/face/blender/__init__.py
+++ HRI/face/blender/__init__.py
@@ -21,7 +21,8 @@ import comm
 PREFIX="OB"
 EYES_MAX_ANGLE=30
 TIME_STEP=1/GameLogic.getLogicTicRate()
-	
+SH_ACT_LEN=50
+
 def check_actuators(cont, acts):
     """Check required actuators are ok."""
     for act in acts:
@@ -40,10 +41,10 @@ def set_eyelids(srv_face, time_step):
     """This function is for demo purposes only and do not use a proper interface
     """
     factor = float(GameLogic.eyes[0].orientation[2][1]) + .505
-    srv_face.conflict_solver.set_AU('gaze', '43R', 0.9-factor, time_step)
-    srv_face.conflict_solver.set_AU('gaze', '43L', 0.9-factor, time_step)
-    srv_face.conflict_solver.set_AU('gaze', '07R', factor, time_step)
-    srv_face.conflict_solver.set_AU('gaze', '07L', factor, time_step)
+    GameLogic.getCurrentController().owner['p43L'] = 0.9-factor * SH_ACT_LEN
+    GameLogic.getCurrentController().owner['p43R'] = 0.9-factor * SH_ACT_LEN
+#    srv_face.conflict_solver.set_AU('gaze', '43', 0.9-factor, time_step)
+    srv_face.conflict_solver.set_AU('gaze', '07', factor, time_step)
 
 
 def initialize():
@@ -52,12 +53,7 @@ def initialize():
 
     import logging
     logging.basicConfig(level=logging.WARNING, format=comm.FORMAT)
-    
-    objs = GameLogic.getCurrentScene().objects
-    GameLogic.eyes = (objs[PREFIX+"eye-R"], objs[PREFIX+"eye-L"])
-    GameLogic.empty_e = objs[PREFIX+"Empty-eyes"]
-    GameLogic.empty_e['updated'] = False
-    
+
     cont = GameLogic.getCurrentController()
     
     import conf
@@ -66,60 +62,28 @@ def initialize():
         raise Exception("WARNING: missing definitions %s in config file:" %\
                             (missing, conf.file_loaded))
     
-    import gaze
-    GameLogic.srv_gaze = comm.createServer(gaze.Gaze, gaze.GazeClient, conf.conn_gaze)
-    
     import face
-    # make sure we have the same Action Units (Blender Shape Actions)
     GameLogic.srv_face = comm.createServer(face.Face, face.FaceClient, conf.conn_face)
+    # set available Action Units from the blender file (Blender Shape Actions)
     acts = [act for act in cont.actuators if
             not act.name.startswith('-') and act.action]
     GameLogic.srv_face.set_available_AUs([act.name for act in acts])
     # override actuators mode
     check_actuators(cont, acts)
 
-    import affect
-    #XXX: faster way: update face directly.
-    GameLogic.srv_affect = comm.createServer(affect.Affect, affect.AffectClient, conf.conn_affect)
-
     import threading
-    threading.Thread(name='gaze', target=GameLogic.srv_gaze.serve_forever).start()
     threading.Thread(name='face', target=GameLogic.srv_face.serve_forever).start()
-    threading.Thread(name='affect', target=GameLogic.srv_affect.serve_forever).start()
+    # for demo purposes only
+    objs = GameLogic.getCurrentScene().objects
+    GameLogic.eyes = (objs[PREFIX+"eye-R"], objs[PREFIX+"eye-L"])
+    GameLogic.empty_e = objs[PREFIX+"Empty-eyes"]
+    GameLogic.empty_e['updated'] = False
+
+    print "BGE logic running at", GameLogic.getLogicTicRate(), "fps."
 
     # ok, startup
     GameLogic.initialized = True	
     cont.activate(cont.actuators["- wakeUp -"])
-    set_eyelids(GameLogic.srv_face, 0)
-
-	
-
-def update_eyes(srv_gaze):
-    """To get a smooth movement (just linear), we start the eye movement,
-        next iteration shall continue."""
-
-    if GameLogic.empty_e['updated']:       # handle empty_e when moved
-        GameLogic.empty_e['updated'] = False
-        srv_gaze.set_focus(GameLogic.empty_e.localPosition)
-    else:                               # or when commands were sent to server
-        srv_gaze.update(TIME_STEP)
-
-    if not srv_gaze.changed:
-        return
-    elif srv_gaze.changed == 'f':   # focus
-        GameLogic.empty_e.worldPosition = srv_gaze.focus
-    elif srv_gaze.changed == 'o': # orientation
-        import Mathutils
-        o_angle, o_vect = srv_gaze.orientation[1:]
-        # angle is normalized, we need it in degrees here, +
-        #  see TODO: eye texture orientation.
-        if o_angle == .0:
-            o_vect = (.0,.0,.0001)
-        oMatrix = Mathutils.RotationMatrix(EYES_MAX_ANGLE*o_angle-180, 3, "r",
-                                           Mathutils.Vector(*o_vect))
-        GameLogic.eyes[0].setOrientation(oMatrix)
-        GameLogic.eyes[1].setOrientation(oMatrix)
-        #DBG: print "L eye orientation now:", GameLogic.eyes[0].getOrientation()
     set_eyelids(GameLogic.srv_face, 0)
 
 
@@ -127,7 +91,7 @@ def update_face(srv_face, cont):
     for au, infos in srv_face.update(TIME_STEP):
         target_val, duration, elapsed, value = infos
 #        print "setting property p"+au+" to value", value
-        cont.owner['p'+au] = value * 50
+        cont.owner['p'+au] = value * SH_ACT_LEN
         cont.activate(cont.actuators[au])
 #TODO: check why 43R is a valid key, and what is the value ?
 # print cont.owner['43R'], cont.owner['p43R']
@@ -148,5 +112,8 @@ def main():
             cont.activate(cont.actuators["- QUITTER"])
             raise
 
-    update_eyes(GameLogic.srv_gaze)
+#   update eyes
+    if GameLogic.empty_e['updated']:       # handle empty_e when moved
+        set_eyelids(GameLogic.srv_face, TIME_STEP)
+        GameLogic.empty_e['updated'] = False
     update_face(GameLogic.srv_face, cont)
