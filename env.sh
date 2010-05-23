e40ca82058301b3e4782968539753be74dcc0fd7
Frédéric Delaunay <fdelauna@qatar.cmu.edu>
2010-05-23 20:15:25 +0300

    added edit_face alias for quickly start-up and testing within blender

--- env.sh
+++ env.sh
@@ -2,5 +2,7 @@ if test -z "$CONCEPT_DIR"; then
     echo '$CONCEPT_DIR' not set, assuming '$PWD' "($PWD)"
     CONCEPT_DIR=$PWD
 fi
-export PYTHONPATH=$PYTHONPATH:$CONCEPT_DIR/common/
+export PYTHONPATH=$PYTHONPATH:$CONCEPT_DIR/common/:$CONCEPT_DIR/HRI/:$CONCEPT_DIR/HRI/face/
 export LIGHTBOT_CONF=$CONCEPT_DIR/lightbot.conf
+
+alias edit_face="blender $CONCEPT_DIR/HRI/face/blender/lighthead.blend"
