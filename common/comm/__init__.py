322a1981234f1d0a730dae6c89416c8855eeca22
Frédéric Delaunay <fdelauna@qatar.cmu.edu>
2010-05-25 14:15:46 +0300

    added GPLv3 header

--- common/comm/__init__.py
+++ common/comm/__init__.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 
-#
-# Copyright 2008 Frederic Delaunay, f dot d at chx-labs dot org
-#
-#  This file is part of the comm module for the concept project: 
-#   http://www.tech.plym.ac.uk/SoCCE/CONCEPT/
-#
-#  comm module is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  comm module is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
+# Lighthead-bot programm is a HRI PhD project at
+#  the University of Plymouth,
+#  a Robotic Animation System including face, eyes, head and other
+#  supporting algorithms for vision and basic emotions.  
+# Copyright (C) 2010 Frederic Delaunay, frederic.delaunay@plymouth.ac.uk
+
+#  This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 comm package for python.
 versions >= 2.5
--- common/conf_example.py
+++ common/conf_example.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+# Lighthead-bot programm is a HRI PhD project at
+#  the University of Plymouth,
+#  a Robotic Animation System including face, eyes, head and other
+#  supporting algorithms for vision and basic emotions.  
+# Copyright (C) 2010 Frederic Delaunay, frederic.delaunay@plymouth.ac.uk
+
+#  This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import conf
 
 if not conf.is_complete:
