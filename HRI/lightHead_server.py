6581e8bbcb8cd4031105afc4817d813ae6da5f78
fred <frederic.delaunay@plymouth.ac.uk>
2010-11-29 18:56:55 +0000

    added 1st support for feature pool, query server with command get_snapshot, argument can specify an specific Origin

--- HRI/lightHead_server.py
+++ HRI/lightHead_server.py
@@ -1,3 +1,37 @@
+# LightHead-bot programm is a HRI PhD project at 
+#  the University of Plymouth,
+#  a Robotic Animation System including face, eyes, head and other
+#  supporting algorithms for vision and basic emotions.  
+# Copyright (C) 2010 Frederic Delaunay, frederic.delaunay@plymouth.ac.uk
+
+#  This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#
+# SERVER MODULE
+#
+# This module listen to control connections and dispatches module-specific
+#  commands to the appropriate submodule. Also allows retreiving a snapshot of
+#  the current context.
+#
+# MODULES IO:
+#============
+# OUTPUT: All HRI modules
+#
+# INPUT: - learning (for context retrieval)
+#
+
 import logging
 
 from comm.meta_server import MetaRequestHandler, MetaServer
@@ -10,6 +44,31 @@ ORGN_LIPS = 'lips'
 ORGN_HEAD = 'head'
 ORIGINS = (ORGN_GAZE, ORGN_FACE, ORGN_LIPS, ORGN_HEAD)
 
+
+class FeaturePool(object):
+    """This class serves as a short term memory. It holds all possible features
+    so other modules can query a snapshot of the current robot's state."""
+
+    def __init__(self):
+        self.context = {}       # identifier : value(s)
+
+    def is_empty(self):
+        return not self.context
+
+    def get_snapshot(self, features=None):
+        """Get a snapshot, optionally selecting specific features.
+        features == None: return whole context
+        features == iterable: return subset of the context.
+        """
+        print features, self.context
+        if not features:
+            return self.context
+        return dict( (f,self.context[f]) for f in features )
+
+    def set_value(self, identifier, value):
+        self.context[identifier] = value
+
+
 class lightHeadHandler(MetaRequestHandler):
     """Handles high level protocol transactions: origin and commit"""
 
@@ -20,27 +79,43 @@ class lightHeadHandler(MetaRequestHandler):
             self.handlers[origin] = self.create_subhandler(*srv_hclass)
 
     def cmd_origin(self, argline):
-        if not argline:
-            self.send_msg("origin is %s" % self.curr_handler)
-        else:
+        """Set or Send current origin/subhandler"""
+        if argline:
             self.set_current_subhandler(self.handlers[argline])
+        else:
+            self.send_msg("origin is %s" % self.curr_handler)
 
     def cmd_commit(self, argline):
+        """Marks end of a transaction"""
         for key in ORIGINS:
             if self.handlers.has_key(key):
                 self.handlers[key].cmd_commit(argline)
 
     def cmd_reload(self, argline):
-        """reload subserver modules"""
+        """Reload subserver modules"""
         self.send_msg('TODO')
+
+    def cmd_get_snapshot(self, argline):
+        """Returns the current snapshot of robot context"""
+        if self.server.FP.is_empty():
+            for origin in self.server.origins.iterkeys():
+                subserver = self.server.get_server(origin)
+                self.server.FP.set_value(origin, subserver.get_features(origin))
+        for k,v in self.server.FP.get_snapshot(argline.split()).iteritems():
+            if v and hasattr(v, '__iter__'):
+                v = [ str(i) for i in v ]
+            self.send_msg(k+' '+' '.join(v))
+        self.send_msg('end_snapshot')
 
 
 class lightHeadServer(MetaServer):
     """Sets and regroups subservers of the lightHead system."""
 
     def __init__(self):
+        """All subServers will receive a reference to the Feature Pool"""
         MetaServer.__init__(self)
         self.origins = {}       # { origin: index in self.servers and handlers }
+        self.FP = FeaturePool() # the feature pool for context queries
 
     def __getitem__(self, protocol_keyword):
         return self.origins[protocol_keyword][0]
@@ -48,6 +123,9 @@ class lightHeadServer(MetaServer):
 
     def get_handler(self, keyword):
         return self.origins[keyword][1]
+
+    def get_server(self, keyword):
+        return self.origins[keyword][0]
 
     def register(self, server, request_handler_class, origin):
         """bufferize server-handler associations with origin keyword."""
@@ -58,7 +136,6 @@ class lightHeadServer(MetaServer):
                   server, request_handler_class, origin)
         MetaServer.register(self, server, request_handler_class)
         self.origins[origin] = self.servers_SHclasses[-1]
-
 
     def create_protocol_handlers(self):
         """Bind individual servers and their handler to the meta server."""
