d0e6f7fd83d0e99a3bc5bc7c766d9806648e43b2
DFred <frederic.delaunay@plymouth.ac.uk>
2010-12-12 18:22:12 +0000

    removed obsolete file + added University of Plymouth new face

--- common/conf_required_lightHead.py
+++
@@ -1,5 +0,0 @@
-# This simple file defines what configuration entries are required for the
-#  system to work.
-
-REQUIRED= ('conn_face',) # connection to face server: ('ip_address', port)
-#           'conn_gaze')
