c2cef2b8c6f13c1476bbe704ce9af7fa7678e4c4
fred <frederic.delaunay@plymouth.ac.uk>
2011-02-06 15:48:41 +0000

    moved feature pool in HRI.__init__.py

--- HRI/__init__.py
+++ HRI/__init__.py
@@ -1,3 +1,46 @@
+import numpy as np
+
+class FeaturePool(dict):
+    """This class serves as a short term memory. It holds all possible features
+    so other modules can query a snapshot of the current robot's state.
+    Also, it's a singleton.
+    """
+    # single instance holder
+    instance = None
+
+    def __new__(cls):
+        """Creates a singleton.
+        Another feature pool? Derive from that class overriding self.instance,
+         and don't bother with the __ prefix to make it pseudo-private...
+        cls: don't touch (it's the current type, ie: maybe a derived class type)
+        """
+        if cls.instance is None:
+            cls.instance = super(FeaturePool,cls).__new__(cls)
+        return cls.instance
+
+    def __setitem__(self, name, np_array):
+        """Registers a new Feature into the pool.
+        name: string identifying the feature
+        np_array: numpy.ndarray (aka numpy array) of arbitrary size
+        """
+        LOG.debug("new feature in the pool from %s: %s", name, np_array)
+        if np_array is not None:
+            assert isinstance(np_array,np.ndarray),\
+                'Not a numpy ndarray instance'
+        dict.__setitem__(self, name, np_array)
+
+    def get_snapshot(self, features=None):
+        """Get a snapshot, optionally selecting specific features.
+        features: iterable of str specifying features to be returned.
+        Returns: all context (default) or subset from specified features.
+        """
+        features = features or features.iterkeys()
+        return dict( (f,
+                      isinstance(self[f],np.ndarray) and self[f] or 
+                      self[f].get_feature() )
+                     for f in features )
+
+
 def initialize(thread_info):
     """Initialize the system.
     thread_info: tuple of booleans setting threaded_server and threaded_clients
@@ -22,6 +65,6 @@ def initialize(thread_info):
     server = comm.create_server(lightHeadServer, lightHeadHandler,
                                 conf.lightHead_server, thread_info)
     # Because what we have here is a *meta server*, we need to initialize it
-    #  properly; face and all other subservers are initialized in that call
+    #  properly; face and all other subservers are initialized in that call.
     server.create_protocol_handlers()
     return server
