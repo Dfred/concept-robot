8a5b7e891e9ac73cce9411129ddd4e1918c8efe9
fred <frederic.delaunay@plymouth.ac.uk>
2011-02-09 20:53:53 +0000

    additional features but is not yet done

--- experiments/blink-influence/expression-player.py
+++ experiments/blink-influence/expression-player.py
@@ -17,6 +17,7 @@
 
 import math
 import time
+import random
 
 import control
 import vision
@@ -33,6 +34,7 @@ __maintainer__ = "Frédéric Delaunay"
 __email__ = "frederic.delaunay@plymouth.ac.uk"
 __status__ = "Prototype" # , "Development" or "Production"
 
+
 def error(msg):
     print msg
     exit(1)
@@ -45,16 +47,20 @@ class IntelligentPlayer():
     def finish(self):
         """Called on 'STOPPED' state.
         """
+        print 'finishiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiing', self.name
         return None
 
     def read_section(self):
         """
-        Returns: 'EOSECTION','FINISHING'
+        Returns: 'EOSECTION','STOPPED'
         """
         line = self.performance.readline()
+        if line.startswith('EOSECTION'):
+            return 'EOSECTION'
         if not line:
-            return 'FINISHING'
+            return 'STOPPED'
         self.comm_expr.send_msg(line)
+        self.wait_comm_expr_reply()
 
     def listenTo_participant(self):
         """
@@ -68,18 +74,27 @@ class IntelligentPlayer():
         """
         Returns: 'REPLIED'
         """
+        line = random.Random().choice(self.replies['rep'])
+        self.comm_expr.send_msg(line)
+        self.wait_comm_expr_reply()
         return 'REPLIED'
     
     def nodTo_participant(self):
         """
         Returns: 'REPLIED'
         """
+        line = random.Random().choice(self.replies['nod'])
+        self.comm_expr.send_msg(line)
+        self.wait_comm_expr_reply()
         return 'REPLIED'
     
     def interrupt_participant(self):
         """
         Returns: 'REPLIED'
         """
+        line = random.Random().choice(self.replies['int'])
+        self.comm_expr.send_msg(line)
+        self.wait_comm_expr_reply()
         return 'REPLIED'
     
     def search_participant(self):
@@ -87,20 +102,48 @@ class IntelligentPlayer():
         Returns: 'FOUND_PART'
         """
         self.vision.update()
-        faces = self.vision.find_faces()
+        self.faces = self.vision.find_faces()
         if self.vision.gui:
-            self.vision.mark_faces(faces)
+            self.vision.mark_areas(self.faces)
             self.vision.gui.show_frame(self.vision.frame)
-        return faces and 'FOUND_PART' or None
+        return self.faces and 'FOUND_PART' or None
 
     def adjust_head(self):
         """
         Returns: 'ADJUSTED'
         """
+        area = self.faces[0]
+        relative_x = (self.vision_area[0] - (area.x + (area.w/2.0)) )
+        relative_y = (self.vision_area[1] - (area.y + (area.h/2.0)) )
+        gaze = (0,0,0)#self.follow_face_with_gaze(relative_x, relative_y)
+        neck = (gaze,(0,0,0))#self.follow_face_with_neck(relative_x, relative_y, gaze[1])
+        # reuse numbers from Joachim's magic hat ;P
+        face_distance = ((-88.4832 * math.log(self.vision_area[0])) + 538.3782)
+        self.comm_expr.send_neck_gaze(gaze, neck)
+#        self.wait_comm_expr_reply()
         return 'ADJUSTED'
 
-    def __init__(self):
+    def wait_comm_expr_reply(self):
+        """Wait for a reply from the server.
         """
+        while self.comm_expr.status is None:
+            time.sleep(0.05)
+
+    def read_replies(self):
+        """
+        """
+        with file('./performance_replies.txt', 'r', 1) as f:
+            for i,l in enumerate(f.readlines()):
+                try:
+                    group, line = l.split('  ')
+                    self.replies[group].append(line)
+                except Exception, e:
+                    print 'error with replies file line %i: %s (%s)' % (i,l,e)
+                    exit(1)
+
+    def __init__(self, dev_index):
+        """
+        dev_index: camera device index
         """
         PLAYER_DEF = ( (('FOUND_PART', 'REPLIED'), self.read_section),
                        ('EOSECTION',  self.listenTo_participant),
@@ -115,12 +158,19 @@ class IntelligentPlayer():
                             ('STOPPED', self.finish),
                         )
                       
-        self.player = control.Behaviour(PLAYER_DEF)
-        self.tracker = control.Behaviour(FACETRACKER_DEF, self.player)
+        self.player = control.Behaviour('player', PLAYER_DEF)
+        self.tracker = control.Behaviour('tracker',FACETRACKER_DEF, self.player)
         conf.load()
-        self.vision = vision.CamFaceFinder(conf.haar_cascade_path)
-        self.vision.gui_create()
+        try:
+            self.vision = vision.CamFaceFinder(conf.haar_cascade_path,dev_index)
+            self.vision_area = self.vision.get_resolution()
+            self.vision.gui_create()
+        except vision.VisionException, e:
+            print e
+            exit(1)
         self.performance = file('./performance.txt', 'r', 1)
+        self.replies = {'rep' : [], 'int': [], 'nod' : []}
+        self.read_replies()
         self.comm_expr = ExpressionComm(conf.expression_server)
 
     def cleanup(self):
@@ -153,8 +203,14 @@ class IntelligentPlayer():
 
 
 if __name__ == '__main__':
+    import sys
+    try:
+        dev_index = len(sys.argv) > 1 and int(sys.argv[-1]) or 0
+    except ValueError:
+        print 'argument shall be the camera device index (as an integer).'
+        exit(1)
     # in this scenario, we are a process using at least the webcam (and audio).
-    player = IntelligentPlayer()
+    player = IntelligentPlayer(dev_index)
     player.run()
     player.cleanup()
     print 'done'
