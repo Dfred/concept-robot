93641e8a051873c9706b71c86b707e9c68b2d9a6
joachim <Joachim.deGreeff@plymouth.ac.uk>
2011-10-25 12:37:09 +0100

    test_behaviour now runs in own thread and can receive data from vision GUI

--- HMS/test_behaviour.py
+++ HMS/test_behaviour.py
@@ -1,5 +1,7 @@
 
 import sys
+import Queue
+import threading
 from HMS import cogmod
     
 from HMS import expression_player as ep
@@ -7,53 +9,84 @@ from utils import conf, handle_exception, LOGFORMATINFO
 from utils.FSMs import SMFSM
 from HMS.communication import ExpressionComm
 
-
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from cogmod.layout import Ui_MainWindow
-from cogmod import cfg, agent, lg, graphic
+from cogmod import graphic
+
+
+class Behaviour_thread(threading.Thread):
+    
+    def __init__(self, comm_queue):
+        threading.Thread.__init__(self)
+        self.player = FollowBehaviour_Builder(comm_queue)
+    
+    def run(self):
+        self.player.run()
+        
+    def end(self):
+        self.player.cleanup()
 
 
 
-class TestBehaviour_Builder(ep.Behaviour_Builder):
+class FollowBehaviour_Builder(ep.Behaviour_Builder):
     """
     """
-    def start(self):
+    
+    def __init__(self, comm_queue):
+        
+        self.comm_queue = comm_queue
+        rules = ((SMFSM.STARTED,self.started), ('DETECT',  self.detect), (SMFSM.STOPPED,self.stopped) )
+        machine_def = [ ('test', rules, None) ]
+        ep.Behaviour_Builder.__init__(self, machine_def, with_vision=False)
+    
+
+    def started(self):
         print 'test started'
         return 'DETECT'
         
         
     def detect(self):
-#        app = QApplication(sys.argv)
-#        mainwindow = graphic.GUI()
-#        ui = Ui_MainWindow()
-#        ui.setupUi(mainwindow)
-#        mainwindow.layout = ui
-#        mainwindow.set_defaults()
-#        mainwindow.show()
-#        sys.exit(app.exec_())
-
-        print self.comm_expr.status
-
-        return SMFSM.STOPPED
+        print "detecting"
         
-          
+        self.comm_expr.set_gaze((0.1, 0.5, 0.0))
+        self.comm_expr.send_datablock("Test")
+                
+        while True:
+            
+            try:
+                item = self.comm_queue.get(False)
+            except Queue.Empty:
+                item = None
+                
+            if item == "quit_fsm":
+                return SMFSM.STOPPED
+            elif item:
+                print item
+                
+        
     def stopped(self, arg):
         print 'test stopped'
         return
       
-    def __init__(self):
-        rules = ((SMFSM.STARTED,self.start), ('DETECT',  self.detect), (SMFSM.STOPPED,self.stopped) )
-        machine_def = [ ('test', rules, None) ]
-        ep.Behaviour_Builder.__init__(self, machine_def, with_vision=False)
 
 
 if __name__ == '__main__':
-    #import logging
-    #logging.basicConfig(level=logging.DEBUG,**LOGFORMATINFO)
-    player = TestBehaviour_Builder()
-    player.run()
-    player.cleanup()
+    import logging
+    logging.basicConfig(level=logging.DEBUG,**LOGFORMATINFO)
+    comm_queue = Queue.Queue()
+    
+    bt = Behaviour_thread(comm_queue)
+    bt.start()
+    
+    app = QApplication(sys.argv)
+    mainwindow = graphic.GUI(comm_queue)
+    ui = Ui_MainWindow()
+    ui.setupUi(mainwindow)
+    mainwindow.layout = ui
+    mainwindow.set_defaults()
+    mainwindow.show()
+    sys.exit(app.exec_())
     
     
     
