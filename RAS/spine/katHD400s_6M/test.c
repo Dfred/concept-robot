97dcb029240e9f48c9c04b7d13610937532a5117
DFred <frederic.delaunay@plymouth.ac.uk>
2011-11-24 16:14:43 +0000

    updated test to show hardware limits

--- RAS/spine/katHD400s_6M/test.c
+++ RAS/spine/katHD400s_6M/test.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 #include <stdlib.h>	// exit, abs
-#include <stdio.h>	// printf
+#include <stdio.h>	// printf, fflush
 #include <unistd.h>	// scanf
 #include <math.h>	// cos,..
 #include <string.h>	// memcpy
@@ -13,13 +13,17 @@
 #define MAX_SPEED 180	// in encoders per 10 ms => 18 000 encoders/s.
 #define ACCEL	2	// 1: long accel (~sqrt) / 2: short accel (~linear)
 
+#define NBR_AXIS 6
+
 #define ERROR_FAC 1	// constant factor coming from the blue
 #define GET_TIME(st) if (gettimeofday(&st, NULL) == -1) {printf("error gettimeofday\n");}
 
-void print_status(int encoders[6], int velocities[6], char self_overwrite)
+void print_status(int encoders[NBR_AXIS],
+		  int velocities[NBR_AXIS],
+		  char self_overwrite)
 {
   printf("%cMotors: ", self_overwrite ? '\r':' ');
-  for (int i=0; i<6; i++)
+  for (int i=0; i<NBR_AXIS; i++)
     printf("(#%i : %ie %iv) ", i+1, encoders[i], velocities[i]);
   if (! self_overwrite)
     printf("\n");
@@ -88,7 +92,7 @@ int get_speed(float total_dist, float total_dur,
 int
 calibrate_if_needed()
 {
-  int encoders[6];
+  int encoders[NBR_AXIS];
   printf("-- checking if blocked.\n");
   switch (is_blocked())
     {
@@ -97,7 +101,7 @@ calibrate_if_needed()
     }
   getEncoders(encoders);
   printf("-- checking if calibration is needed.\n");
-  for (int axis = 1; axis <= 6; axis++)
+  for (int axis = 1; axis <= NBR_AXIS; axis++)
     if (moveMot(axis, encoders[axis-1], 10, ACCEL) == ERR_FAILED)
       {
 	calibrate();
@@ -109,19 +113,23 @@ calibrate_if_needed()
 
 int main(int ac, char *argv[])
 {
-  int encoders[6], velocities[6];
+  int encoders[NBR_AXIS], velocities[NBR_AXIS];
   if (ac < 2)
     { printf("config file argument required.\n"); exit(1); }
+
+  printf("Initializing Katana..."); fflush(stdin);
   if (initKatana(argv[1], "192.168.168.232") == -1)
     { printf("initKatana failed\n"); exit(1); }
+  printf("done.\n");
   if (calibrate_if_needed() == -1)
       exit(1);
 
-  float fac = 1;
-  if (sscanf(argv[ac-1], "%f", &fac) == -1)
-    { printf("invalid factor: bad float value: %s\n", argv[ac-1]); exit(1); }
-  else
-    printf("Using factor %f\n", fac);
+  int axis_min[NBR_AXIS], axis_max[NBR_AXIS], axis_EPC[NBR_AXIS];
+  if (getAxisMinMaxEPC(axis_min, axis_max, axis_EPC) == -1)
+    { printf("getAxisMinMaxEPC failed\n"); exit(1); }
+  for (int i = 0; i < NBR_AXIS; i++)
+    printf("Axis #%i encoders [%i,%i] , %i encoders per cycle\n", i+1,
+	   axis_min[i], axis_max[i], axis_EPC[i]);
 
   int enc_base = 0, enc_diff = 0, axis = 0, secs = 0, speed = 0, updates = 0;
   struct timeval t, t_last, t_res, t_base, t_target, t_end;
@@ -170,7 +178,7 @@ int main(int ac, char *argv[])
       print_status(encoders, velocities, 0);
       printf("\naxis, target, seconds > ");
       if (scanf("%i %i %i", &axis, &enc_diff, &secs) == EOF) break;
-      if (axis < 1 || axis > 6) continue;
+      if (axis < 1 || axis > NBR_AXIS) continue;
 
       enc_base = encoders[axis-1];
       enc_diff -= enc_base;				// now a relative value
