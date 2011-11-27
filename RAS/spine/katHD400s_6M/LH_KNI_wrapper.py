04b99fdeab60aa4b9e2ff83e3b1abefc6a937b30
DFred <frederic.delaunay@plymouth.ac.uk>
2011-11-27 22:22:33 +0000

    added TPos ctypes.Structure

--- RAS/spine/katHD400s_6M/LH_KNI_wrapper.py
+++ RAS/spine/katHD400s_6M/LH_KNI_wrapper.py
@@ -20,27 +20,41 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-LightHead KNI wrapper using Python ctypes.
+ LightHead KNI wrapper using Python ctypes.
 This file can only be used with LH_KNI_wrapper.cpp compiled to a shared library.
 Refer to the README for further information.
-
-Only basic control on the arm is needed. Avoiding swig also allows us to be
-closer to low-level apis, hence reducing overhead.
+Avoiding KNI's swig wrapper gives us freedom to extend KNI's high level API and
+maintain a human-friendly python interface.
 """
 from os.path import dirname, sep
 from platform import system
-from ctypes import *
+from ctypes import CDLL, c_int, c_double, Structure
 
 from RAS.spine import SpineError
 
 class LHKNI_wrapper(object):
-    """
+    """ctypes KNI wrapper.
     """
     @staticmethod
     def validate(API_return_value):
+        """Checker raising SpineError Exception upon KNI API error.
+        """
         if API_return_value == -1:
             raise SpineError('KNI failure')
         return API_return_value
+
+    class TPos(Structure):
+        """KNI TPos in ctypes.
+        """
+        _fields_ = [("X", c_double),
+                    ("Y", c_double),
+                    ("Z", c_double),
+                    ("phi",   c_double),
+                    ("theta", c_double),
+                    ("psi",   c_double)]
+        def __str__(self):
+            return str("pos%s rot%s" % ( (self.X,self.Y,self.Z),
+                                         (self.phi,self.theta,self.psi)) )
 
     #XXX: Careful: it seems ctypes has issues with debug symbols!
     LIB_PATH = dirname(__file__)+sep+'libLH_KNI_wrapper'+(
