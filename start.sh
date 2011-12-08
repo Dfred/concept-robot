5e00f8b4fa221c7c36d271662753feac7c8947d4
lightHead <lightHead@concept>
2011-12-08 18:59:50 +0000

    proper options parsing

--- start.sh
+++ start.sh
@@ -57,21 +57,20 @@ case `uname -s` in
         ;;
 esac
 
-getopts "w" OPTS
-if [ "$OPTS" = "w" ]; then
-    shift;
-    if [ "$OPTS" = "w" ]; then
+OPTIONS="wi"
+OPT="-"
+while [ "$OPT" != "?" ]
+do getopts $OPTIONS OPT
+case "$OPT" in
+    "w")
     	PROJECT_NAME=$PROJECT_NAME-window
-    fi
-fi
-
-getopts "i" OPTS
-if [ "$OPTS" = "i" ]; then
-    shift;
-    if [ "$OPTS" = "i" ]; then
-    	PREFIX="optirun ./"
-    fi
-fi
+	;;
+    "i")
+	PREFIX="optirun ./"
+	;;
+esac
+done
+shift
 
 if ! test -x ./$PROJECT_NAME$BIN_SUFFIX; then
     echo "Could not find executable file '$PROJECT_NAME' in this directory."
@@ -79,12 +78,13 @@ if ! test -x ./$PROJECT_NAME$BIN_SUFFIX; then
 fi
 
 if test -z "$CONF_FILE"; then
+    echo "Missing config file."
     exit 3
 fi
 
 # Now launch
 
-echo -n "--- launching face ---"
+echo -n "--- launching face --- : $PREFIX$PROJECT_NAME$BIN_SUFFIX $@"
 if [ $# -ge 1 ]; then echo "using options: $@"; else echo "";
 fi
 
