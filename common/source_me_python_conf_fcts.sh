01d78cd8afb719e583f253534b215f3387542ce3
Frederic Delaunay <frederic.delaunay@plymouth.ac.uk>
2012-04-04 22:30:55 +0100

    recovering file lost in merge

---
+++ common/source_me_python_conf_fcts.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# just checks if conf can be loaded automatically (env / filepath)
+get_CWD ()
+{
+python -c 'import os; print os.getcwd()'
+}
+
+get_paths ()
+{
+    python -c 'import sys; print sys.path'
+}
+
+get_python_conf ()
+{
+python -c 'from utils import conf;
+conf.set_name("'$1'")
+try:
+ missing = conf.load()
+except conf.LoadException, e:
+ print "CONFIGURATION ERROR:", e[1], "(file: %s)" % e[0]
+ print "candidate files:", conf.build_candidates()
+ exit(1)
+print conf.__LOADED_FILE
+exit(0)'
+}
+
+# checks if there's missing definitions in the conf file
+check_python_conf ()
+{
+python -c 'from utils import conf;
+conf.set_name("'$1'")
+try:
+ missing = conf.load()
+except conf.LoadException, e:
+ print "CONFIGURATION ERROR:", e[1], "(file: %s)" % e[0]
+ print "candidate files:", conf.build_candidates()
+ exit(1)
+if missing:
+ print "missing definitions", missing
+ exit(1)
+exit(0)'
+}
+
+# get search path and files checked for conf
+get_python_conf_candidates ()
+{
+python -c 'import conf;
+conf.set_name("'$1'")
+print " ".join(conf.build_candidates())'
+}
