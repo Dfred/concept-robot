3f58830245e661aeda5b1a0be319dbecab91fe50
fred <frederic.delaunay@plymouth.ac.uk>
2012-05-18 19:01:32 +0100

    support for dual channel inputs

--- utils/audio/__init__.py
+++ utils/audio/__init__.py
@@ -1,8 +1,9 @@
 import math
 import pyaudio
 import logging
+from array import array
 from utils import set_logging_default
-from utils.converters import bytestring2floats
+from utils import conf
 
 LOG = logging.getLogger(__package__)
 set_logging_default(2)
@@ -13,19 +14,28 @@ class Audio(object):
     """
     P = pyaudio.PyAudio()
     INFOS = [P.get_device_info_by_index(i) for i in range(P.get_device_count())]
-    def __init__(self, dev_indices):
+    def __init__(self, sensor_name=None):
         self.streams = {}
         self.datas = []
-        for i in dev_indices:
-            LOG.debug("opening stream %s", self.INFOS[i])
-            sr = int(self.INFOS[i]['defaultSampleRate'])
-            self.streams[i] = self.P.open(rate = sr,
-                                          channels = 1,
-                                          format = pyaudio.paFloat32,
-                                          input = True,
-                                          input_device_index = i,
-                                          frames_per_buffer = sr)
-#        print dir(self.streams.values()[0])
+        if sensor_name:
+            devs = conf.lib_audition[sensor_name]['dev_index']
+            if devs[0] == devs[1]:
+                self.use_device(devs[0], 2)
+            else:
+                self.use_device(devs[0], 1)
+                self.use_device(devs[1], 1)
+        
+    def use_device(self, dev_index, channels):
+        """dev_indices: tuple of one or more inde
+        """
+        LOG.debug("opening stream %s", self.INFOS[dev_index])
+        sr = int(self.INFOS[dev_index]['defaultSampleRate'])
+        self.streams[dev_index] = self.P.open(rate = sr,
+                                              channels = channels,
+                                              format = pyaudio.paFloat32,
+                                              input = True,
+                                              input_device_index = dev_index,
+                                              frames_per_buffer = sr * channels)
             
     def cleanup(self):
         for s in self.streams.values():
@@ -36,22 +46,32 @@ class Audio(object):
     def update_input(self, duration):
         """Can raise IOError, if so device index is in e[0].
         """
+        if len(self.streams) == 1:
+            dev, strm = self.streams.items()[0]
+            d = array('f', strm.read(int(duration*strm._frames_per_buffer)))
+            self.datas = zip( *( (d[i],d[i+1]) for i in range(0,len(d),2) ))
+            return
+        i = 0
         for dev_i, stream in self.streams.items():
             try:
-                stream.data = bytestring2floats( stream.read(
+                self.datas[i] = bytestring2floats( stream.read(
                     int(duration * stream._frames_per_buffer)) )
             except IOError, e:
                 raise IOError(dev_i, e)
+            i += 1
 
-    def set_factor(self, dev_index, factor):
-        LOG.debug("stream #%i: data factor now %f", dev_index, factor)
+    def set_factor(self, factor):
+        LOG.debug("data factor now %f", factor)
         self.f1 = factor
+
+    def get_datas(self):
+        return self.datas
 
     def get_asymetry(self, duration):
         self.update_input(duration)
-        s0, s1 = self.streams.values()
-        return (math.sqrt( sum(s**2 for s in s0.data) / len(s0.data) ) -
-                math.sqrt(sum((s*self.f1)**2 for s in s1.data) / len(s1.data)))
+        dL, dR = self.datas
+        return (math.sqrt(sum( d         **2 for d in dL) / len(dL)) -
+                math.sqrt(sum((d*self.f1)**2 for d in dR) / len(dR)) )
 
 
 if __name__ == "__main__":
@@ -67,13 +87,13 @@ if __name__ == "__main__":
         """
         import time; print "Qeep quiet for a moment please!"; time.sleep(.5)
         f = 1
-        s0, s1 = audio.streams.values()
         while True:
             audio.update_input(.1)
-            fixed_d0 = [ s**2 for s in s0.data ]
-            fixed_d1 = [ (s*f)**2 for s in s1.data ]
-            dBu0 = math.sqrt( sum(fixed_d0) / len(s0.data) )
-            dBu1 = math.sqrt( sum(fixed_d1) / len(s1.data) )
+            s0, s1 = audio.get_datas()
+            fixed_d0 = [ s**2 for s in s0 ]
+            fixed_d1 = [ (s*f)**2 for s in s1 ]
+            dBu0 = math.sqrt( sum(fixed_d0) / len(s0) )
+            dBu1 = math.sqrt( sum(fixed_d1) / len(s1) )
             print "fixed data diff %+.6f, factor %f, dBu diff: %.6f" % (
                 abs(sum(fixed_d0))-abs(sum(fixed_d1)), f, abs(dBu0 - dBu1) )
             if abs(dBu0 - dBu1) < .00001:
@@ -91,11 +111,14 @@ if __name__ == "__main__":
                 dev['defaultSampleRate'],
                 dev['name'], )
 
-    if len(sys.argv) > 1:
-        devs_i = [ int(arg) for arg in sys.argv[1:] ]
-        a = Audio(devs_i)
-        factor = calibrate(a)
-        a.set_factor(devs_i[1], factor)
+    if 1 <= len(sys.argv) < 3:
+        a = Audio()
+        if len(sys.argv) == 2:
+            a.use_device(int(sys.argv[1]), 2)
+        else:
+            a.use_device(int(sys.argv[1]), 1)
+            a.use_device(int(sys.argv[2]), 1)
+        a.set_factor(calibrate(a))
         i, diff = 1, []
         while True:
             if i % 100 == 0:
@@ -108,3 +131,5 @@ if __name__ == "__main__":
                 exit(2)
             print "asymetry: %+f\r" % diff[-1]
             i += 1
+    else:
+        print sys.argv[0]+" [device index [device index]]"
