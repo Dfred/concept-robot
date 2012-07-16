c3bd3b880d5f4870a97a10c21a8547ca186fe8c5
Najibe <najibeb@gmail.com>
2012-07-16 14:23:27 +0100

    Laser Sensor -  suppresion .o et ajouts en-tetes gpl

--- utils/Laser_Sensor/laser_detection.h
+++ utils/Laser_Sensor/laser_detection.h
@@ -1,3 +1,28 @@
+/*
+   LightHead is a programm part of CONCEPT, a HRI PhD project at the University
+   of Plymouth. LightHead is a Robotic Animation System including face, eyes,
+   head and other supporting algorithms for vision and basic emotions.
+   Copyright (C) 2010-2011 Frederic Delaunay, frederic.delaunay@plymouth.ac.uk
+
+   This program is free software: you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Author : Najibe BOUZIDI
+  Date   : 16/07/2012
+  email  : najibe.bouzidi@free.fr
+
+*/
+
 
 typedef struct
         event {float medium_angular_position;
