bef41a1822190f771f947b71d98f41cb58806f58
Najibe <najibeb@gmail.com>
2012-07-16 20:14:59 +0100

    updated path

--- start_shell.bat
+++ start_shell.bat
@@ -1,5 +1,5 @@
 @echo off
 echo.
 IF "%PYTHON%" == "" (echo you must define PYTHON environment variable.) ELSE (
-%PYTHON% utils\readline_client.py localhost 4242 )
+%PYTHON% utils\tools\readline_client.py localhost 4242 )
 pause
