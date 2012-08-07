a007d68fa530349d4084da2b6180f0efe4a03298
fred <frederic.delaunay@plymouth.ac.uk>
2012-08-07 16:37:32 +0100

    more robustness to filenames

--- experiments/blink-influence/start_blink_exp.sh
+++ experiments/blink-influence/start_blink_exp.sh
@@ -1,9 +1,9 @@
 if test -f ./start_blink_exp.sh; then
     cd ../..
 else
-    echo 'you may start this script from the top directory or where you found it'
+    echo 'you should start this script from the directory where you found it'
 fi
-
+EXP_DIR=./experiments/blink-influence/
 source ./common/source_me_to_set_env.sh lightHead
 
-python ./experiments/blink-influence/player_monologue.py $@
+python $EXP_DIR/player_monologue.py -n "$EXP_DIR$@"
