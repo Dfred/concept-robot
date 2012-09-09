78543b32fc0fd3ecd61df348242b4e177604367e
DFred <frederic.delaunay@plymouth.ac.uk>
2012-09-09 12:27:04 +0200

    removed obsoleted script

--- experiments/blink-influence/start_blink_exp.sh
+++
@@ -1,9 +0,0 @@
-if test -f ./start_blink_exp.sh; then
-    cd ../..
-else
-    echo 'you should start this script from the directory where you found it'
-fi
-EXP_DIR=./experiments/blink-influence/
-source ./common/source_me_to_set_env.sh lightHead
-
-python $EXP_DIR/player_monologue.py -n "$EXP_DIR$@"
