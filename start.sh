a128870a3fa0ffe4e0efd5aeebe1f44d19ded889
DFred <frederic.delaunay@plymouth.ac.uk>
2013-08-27 00:05:30 +0100

    better unusual system configuration support

--- start.sh
+++ start.sh
@@ -25,13 +25,24 @@
 #
 
 export PYTHONOPTIMIZE=1	# optimize and also remove docstrings
-BGE_PYTHON_VERS=2.5
+BGE_PYTHON_VERS=2.7
 PROJECT_NAME=lightHead
-#DIST_PACKS_P=/usr/lib/python$BGE_PYTHON_VERS/
-DIST_PACKS_P=$HOME/opt/lib/python$BGE_PYTHON_VERS/
+PROJECT_DIR=`pwd`
+# TODO: array so path separator is set later on
+PROJECT_EXTRA_PATHS="/usr/lib/python$BGE_PYTHON_VERS/dist-packages:./RAS/face/:./extern"
+BLEND_DIR=~/opt/blender-2.49b-linux-glibc236-py26-x86_64
 
-if ! python -c 'print'; then
-    echo "python not found. Did you set your PATH ?"
+if test -z "$PYTHON"; then
+    PYTHON=python$BGE_PYTHON_VERS	#/usr/bin/python3
+    echo "unset \$PYTHON, now set to: $PYTHON"
+fi
+if test -z "$PYTHONHOME"; then
+    PYTHONHOME=/usr/lib/python$BGE_PYTHON_VERS
+    echo "unset \$PYTHONHOME, now set to: $PYTHONHOME"
+fi
+
+if ! $PYTHON -c 'print'; then
+    echo "python has an issue. Did you set \$PYTHON ?"
     exit 1
 fi
 
@@ -40,18 +51,26 @@ if ! test -d "./common"; then
 	exit 1
 fi
 
-PREFIX="./"
-
-OPTIONS="wiW"
+# parsing options
+OPTIONS="widW"
 OPT="-"
+PREFIX="$BLEND_DIR/blenderplayer "
 while [ "$OPT" != "?" ]
 do getopts $OPTIONS OPT
 case "$OPT" in
     "w")
         WINDOW_MODE=1
+	if test -z "$2" || test -z "$3"; then
+	    echo "windowing (-w) option requires width and height arguments"
+	    exit 1
+	fi
+	PREFIX="$PREFIX -w $2 $3 "
 	;;
     "i")
-    	PREFIX="optirun ./"
+    	PREFIX="optirun $PREFIX "
+	;;
+    "d")
+	PREFIX="$PREFIX-d "
 	;;
     "W")
         WITH_REDWINE=1
@@ -60,10 +79,18 @@ case "$OPT" in
 esac
 done
 
+# shortcuts
+alias edit_face="blender $PROJECT_DIR/RAS/face/blender/lightHead.blend"
 
+
+#
 # set environment
+#
 . ./common/source_me_to_set_env.sh
 
+# debugging info
+echo "Python version:" $(get_version)
+#echo "\$PYTHONPATH=$PYTHONPATH"
 
 # handle MinGW and Windows suffix
 case `uname -s` in
@@ -72,7 +99,7 @@ case `uname -s` in
         ;;
     Darwin*)
     	BIN_SUFFIX=".app/"
-    	PREFIX="open "
+    	PREFIX="open $PREFIX "
     	;;
     *)
         BIN_SUFFIX=""
@@ -84,14 +111,13 @@ if test -n "$WINDOW_MODE"; then
     PROJECT_NAME=$PROJECT_NAME-window
 fi
 if test -n "$WITH_REDWINE"; then
-    echo "PYTHONPATH="$PYTHONPATH
-    PREFIX="wine ./"
+    PREFIX="wine $PREFIX "
     BIN_SUFFIX=".exe"
 fi
 
-# sanity checks
+# checking environment variable
 if test -z "$CONF_FILE"; then
-    echo "Missing config file."
+    echo "Missing configuration file."
     exit 3
 fi
 
@@ -101,7 +127,8 @@ if ! test -x ./$PROJECT_NAME$BIN_SUFFIX; then
 fi
 
 # Now launch
-echo "--- launching face --- : $PREFIX$PROJECT_NAME$BIN_SUFFIX " #$@"
+echo "--- launching face --- "
+echo "running: '$PREFIX$PROJECT_NAME$BIN_SUFFIX' " #$@"
 #if [ $# -ge 1 ]; then echo "using options: $@"; else echo ""; fi
 
 $PREFIX$PROJECT_NAME$BIN_SUFFIX
