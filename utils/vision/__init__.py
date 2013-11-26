6ed85eab2a00d388c8e03848fd8cc91e8cad4627
DFred <frederic.delaunay@plymouth.ac.uk>
2013-11-26 16:46:12 +0100

    added debug + towards better docstring style

--- utils/vision/__init__.py
+++ utils/vision/__init__.py
@@ -64,7 +64,8 @@ class CamGUI(object):
     KEY_ESC = 1048603                           #XXX ESC key (at least on linux)
 
     def __init__(self, name='Camera'):
-        """name: window and application name.
+        """
+        >name: window and application name.
         """
         self.name = name
         self.mirror_image = True
@@ -78,8 +79,9 @@ class CamGUI(object):
 
     def show_frame(self, camFrame, delay = 30):
         """
-        camFrame: pyvision camera frame
-        delay: in ms.
+        >camFrame: pyvision camera frame
+        >delay: value in ms.
+        Return: None
         """
         ## cv.WaitKey processes GUI events. value in ms.
         if cv.WaitKey(delay) == self.KEY_ESC:
@@ -98,10 +100,11 @@ class CamGUI(object):
 
     def add_slider(self, label, min_v, max_v, callback):
         """Adds a slider.
-        label: label for the slider.
-        min_v: minimum value for the slider.
-        max_v: maximum value for the slider.
-        callback: function (bound or not) to be called upon slider change.
+        >label: label for the slider.
+        >min_v: minimum value for the slider.
+        >max_v: maximum value for the slider.
+        >callback: function (bound or not) to be called upon slider change.
+        Return: None
         """
         cv.CreateTrackbar(label, self.name, min_v, max_v, callback)
 
@@ -112,7 +115,8 @@ class CamCapture(object):
     """
 
     def __init__(self, sensor_name=None):
-        """sensor_name: if set, calls self.set_camera with that name.
+        """
+        >sensor_name: if set, calls self.set_camera with that name.
         """
         self.cam_info = None
         self.camera = None
@@ -125,30 +129,35 @@ class CamCapture(object):
 
     def set_device(self, dev_index=0, resolution=(800,600)):
         """
-        dev_index: specify camera number for multiple camera configurations.
-        resolution: (width,height) of the frames to grab.
+        >dev_index: specify camera number for multiple camera configurations.
+        >resolution: (width,height) of the frames to grab.
+        Return: None
         """
         self.cam_info = {'dev_index':dev_index, 'resolution':resolution}
 
     def set_camera(self, name):
         """
-        name: identifier of camera as found in conf.
+        >name: identifier of camera as found in conf.
+        Raise: conf.ConfigError
+        Return: None
         """
+        LOG.debug("vision using camera '%s'", name)
         ## Required attributes
         try:
             cam_props = conf.lib_vision[name]
             self.set_device(cam_props['dev_index'], cam_props['resolution'])
         except AttributeError:
-            raise VisionException("Camera '%s' has no definition in your"
+            raise conf.ConfigError("Camera '%s' has no definition in your"
                                    " configuration file." % name)
         except KeyError, e:
-            raise VisionException("Camera '%s' has no '%s' property in your"
-                                  " configuration file." % (name, e))
+            raise conf.ConfigError("Camera '%s' has no '%s' property in your"
+                                   " configuration file." % (name, e))
 
     def set_featurePool(self, feature_pool):
         """Attach the feature_pool for further registration of self.AUs .
         Setting None for our origin puts us in polling mode.
-        feature_pool: a dict of { origin : numpy.array }
+        >feature_pool: a dict of { origin : numpy.array }
+        Return: None
         """
         self.FP = feature_pool
         self.FP['vision'] = None
@@ -161,6 +170,8 @@ class CamCapture(object):
 
     def acquire_camera(self):
         """Grab the camera for work.
+        Raise: VisionException if camera can't be grabbed.
+        Return: None
         """
         self.camera = Webcam(self.cam_info['dev_index'], 
                              self.cam_info['resolution'])
@@ -178,15 +189,21 @@ class CamCapture(object):
         if self.fps_counter:
             self.fps_counter.update()
 
-    def toggle_record(self, filename, fourCodec=None):
+    def toggle_record(self, enable, filename, fourCodec=None):
         """
+        >enable: boolean
+        >filename: "full path of record"
+        >fourCodec: supported codec, default: 'XVID'
+        Return: None
         """
         self.vid_writer = VideoWriterVSP(filename, size=self.camera.size,
                                          fourCC_str=(fourCodec or 'XVID'))
 
-    def toggle_fps(self, val, every_frames=30*2):
+    def toggle_fps(self, enable, every_frames=30*2):
         """
-        every_frames: refresh period in frames
+        >enable: boolean
+        >every_frames: refresh period in frames
+        Return: None
         """
         self.fps_counter = fps.SimpleFPS(every_frames)
 
@@ -196,7 +213,10 @@ class CamCapture(object):
         self.gui = CamGUI()
 
     def gui_write(self, message):
-        """Allow a user message to be displayed"""
+        """Allow a user message to be displayed
+        >message: text string to display
+        Return: None
+        """
         self.gui_message = message
 
     def gui_show(self):
@@ -236,6 +256,9 @@ class CamUtils(CamCapture):
 
     def set_camera(self, name):
         """Overriding to add our properties.
+        >name: camera identifier as found in configuration file.
+        Raise: conf.ConfigError
+        Return: None
         """
         super(CamUtils,self).set_camera(name)
 
@@ -246,8 +269,8 @@ class CamUtils(CamCapture):
             try:
                 setattr(self,prop,conf.lib_vision[name][prop])
             except KeyError, e:
-                raise VisionException("Camera '%s' has no '%s' property in your"
-                                      " configuration file." % (name, e))
+                raise conf.ConfigErrror("Camera '%s' has no '%s' property in "
+                                        "your configuration file." % (name, e))
         assert isinstance(self.XY_factors, tuple) and all(self.XY_factors)
         assert isinstance(self.depth_fct, str)
         ## compile depth_fct
@@ -262,9 +285,9 @@ class CamUtils(CamCapture):
 
     def mark_rects(self, rects, thickness=1, color='blue'):
         """Outlines the given rects in our video stream.
-        rects: absolute Rects.
-        thickness: in pixels.
-        color: string, #rrggbb (in hexa) or color name.
+        >rects: absolute Rects.
+        >thickness: in pixels.
+        >color: string, #rrggbb (in hexa) or color name.
         Return: None
         """
         args = [color]
@@ -279,8 +302,8 @@ class CamUtils(CamCapture):
 
     def mark_points(self, points, color='green'):
         """Outlines the given points in our video stream.
-        points: list of Point instances.
-        color: string, #rrggbb or color name.
+        >points: list of Point instances.
+        >color: string, #rrggbb or color name.
         Return: None
         """
         for p in points:
@@ -289,11 +312,11 @@ class CamUtils(CamCapture):
     def toggle_face_detection(self, enable, haar_cascade_path=None,
                               msize=(50,50), scale=.5):
         """Toggles face detection algorithms.
-        enable: True or False
-        haar_cascade_path: path to .xml, if None then use default
-        msize: (width,height), minimum size
-        scale: float, image scale
-        Returns: None
+        >enable: True or False
+        >haar_cascade_path: path to .xml, if None then use default
+        >msize: (width,height), minimum size
+        >scale: float, image scale
+        Return: None
         """
         LOG.debug("%sabling Face Detection", enable and "En" or "Dis")
         if not enable:
@@ -325,7 +348,7 @@ class CamUtils(CamCapture):
         """Extract the eyes from the list of faces.
          Warning: calling self.update() after find_faces() is not supported!!!
 
-        faces: absolute rects as returned by find_faces().
+        >faces: absolute rects as returned by find_faces().
         Return: list (of list) of the same length as faces. Each sublist
          contains the rect produced by the face detector and the right and left
          eye coordinates produced by the filters.
@@ -336,17 +359,19 @@ class CamUtils(CamCapture):
         """Returns an iterable of gaze vectors (right handeness) from detected
         faces, estimating depth from its width.
 
-        rects: instances or iterable of pyvision Rects as returned by find_faces
+        >rects: instances | iterable of pyvision Rects as returned by find_faces
 
         Poor's man calibration procedure for 'depth_fct' (valid for a specific
         aspect ratio):
         1/ this script gives normalized face's Width, measure distance (meters).
         2/ measure the face Width at different depth levels, i.e. 30cm, 60cm, 1m
-        3/ perform a regression on these values, with x=Width and y=Depth. You can use this website:
+        3/ perform a regression on these values, with x=Width and y=Depth.
+           You can use this website:
 http://people.hofstra.edu/stefan_waner/realworld/newgraph/regressionframes.html
         this will provide a function, e.g. y = 0.249136x^-0.443607
-        4/ in conf's lib_vision, set 'depth_fct' with your function as a string, 
+        4/ in conf's lib_vision, set 'depth_fct' with your function as a string,
         e.g. 'depth_fct': '0.249136*x**-0.443607' for the function above
+        Return: (gaze_vector, ...)
         """
         w, h = float(self.camera.size[0]), float(self.camera.size[1])
         fw, fh = self.XY_factors
