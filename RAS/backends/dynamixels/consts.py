76a3aea80f86907c6e878acead8d09c5ed945713
DFred <frederic.delaunay@plymouth.ac.uk>
2014-03-09 19:15:41 +0100

    cleaning up (mostly backends') name collision with conservative import policy: all __init__.py shall define constants and import only most critical modules + removed obsolete const definitions

--- RAS/backends/dynamixels/consts.py
+++
@@ -1,8 +0,0 @@
-try:
-    import dynamixel
-except ImportError as e:
-    print """dynamixel for python could not be imported.
- Try with easy-install or pip"""
-    sys.exit(EXIT_DEPEND)
-
-BAUD_RATES = sorted([ int(br[5:]) for br in dynamixel.BAUD_RATE.keys() ])
